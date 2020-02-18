8a2f52c35a2c6ba79c2005a9d57b00bde8a789f0
slobod80 <slobod80@gmail.com>
2020-02-18 20:20:33 +0200

    Fix api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,4 +1,13 @@
-const myURL:string="https://cold-water-7357.getsandbox.com/";
+/*const myURL:string="https://cold-water-7357.getsandbox.com/";
 
 export default {myURL}
 
+*/
+
+import axios from "axios"
+
+const ax = axios.create({
+  baseURL: "https://cold-water-7357.getsandbox.com/",
+});
+
+export default ax;
--- src/service/tasksApi.ts
+++ src/service/tasksApi.ts
@@ -1,25 +1,101 @@
 
 import axios from "axios"
-import api from "./api"
+import ax from "./api"
 
+export default class taskApi {
 
-
-/*async function getTasks() {
-	const response=await axios.get("https://cold-water-7357.getsandbox.com/tasks");
-	return response.data;
-}*/
-
-
-
-/*function getTasks() {
-  const promise=tasksApi.getTasks()
+static getTasksFromApi() {
+  const promise=ax.get("tasks")
   .then(response=>{
-  tasksname1.getAllTasks(response);
-    })
+    return response.data;
+  })
   .catch(function(error){alert("Error")});
-}*/
+  return promise;
+}
 
-async function editStatus(payload:any){
+
+static editStatus(payload:any){
+	const response=ax.put("task", 
+		JSON.stringify(
+			{
+			id:payload[0][0],
+			nameOfTask:payload[0][1],
+			myTask:payload[0][2],
+			dateTask:payload[0][3],
+			status:payload[0][4]
+		}),
+		{
+			headers: {'Content-Type': 'application/json'}
+		} )
+	.then(response=>{
+		return response.status;
+	})
+	.catch(response=>{
+		alert("error")
+	})
+	return response;
+}
+
+
+static editTask(payload:any) {
+	const response=ax.put("task", 
+		JSON.stringify(
+			{
+			id:payload[0][0],
+			nameOfTask:payload[0][1],
+			myTask:payload[0][2],
+			dateTask:payload[0][3],
+			status:payload[0][4]
+		}),
+		{
+			headers: {'Content-Type': 'application/json'}
+		} )
+	.then(response=>{
+		return response.status;
+	})
+	.catch(response=>{
+		alert("error")
+	})
+	return response;	
+}
+
+static addTask(id:number,nameOfTask:string,inputTask:string,inputDate:string) {
+	const response=ax.post("add", 
+		JSON.stringify(
+			{
+			id:id,
+			nameOfTask:nameOfTask,
+			myTask:inputTask,
+			dateTask:inputDate,
+			status:"To do"
+		}),
+		{
+			headers: {'Content-Type': 'application/json'}
+		} )
+	.then(response=> {
+		return response.status;
+		})
+	.catch(function(error) {alert(error)});
+	return response;
+}
+
+static deteleTask(index:number) {
+	const response=ax.delete("delete", {data:{ id:index }})
+	.then(response=>{
+		return response.status;
+	})
+	.catch(response=>{
+		alert("error")
+	})
+	return response;
+}
+
+
+
+}
+
+
+/*async function editStatus(payload:any){
 	const response=await axios.put(api.myURL+"task", 
 		JSON.stringify(
 			{
@@ -104,4 +180,4 @@ async function deteleTask(index:number) {
 	return response;
 }
 
-export default {addTask,getTasksFromApi,deteleTask,editTask,editStatus}
+export default {addTask,getTasksFromApi,deteleTask,editTask,editStatus}*/
